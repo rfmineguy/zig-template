31c21a416750a6317a3bcc15a27fbf5eb0ddd8a0
Riley Fischer <rfmineguy@gmail.com>
2026-08-30 22:14:27 -0700

    fix unnoticed compile errors in main

--- src/main.zig
+++ src/main.zig
@@ -7,18 +7,18 @@ const zig_template = @import("zig_template");
 
 pub fn main(init: std.process.Init) !void {
     const row = @embedFile("./examples/row.htpl");
-    const row_t = try Template.load(row, init.io, init.arena);
+    const row_t = try Template.load(row, init.io, init.arena.allocator());
 
     const login = @embedFile("./examples/row.htpl");
-    var login_t = try Template.load(login, init.io, init.arena);
-    try login_t.set("username", .{ .Value = "riley" });
+    var login_t = try Template.load(login, init.io, init.arena.allocator());
+    try login_t.set(".username", .{ .String = "riley" });
 
     const index = @embedFile("./examples/index.htpl");
-    var index_t = try Template.load(index, init.io, init.arena);
-    try index_t.set("header", .{ .Value = "Hi" });
-    try index_t.set("rows", .{ .Embed = login });
-    try index_t.set("header", .{ .Value = "Header" });
-    try index_t.set("some-class", .{ .Value = "card" });
+    var index_t = try Template.load(index, init.io, init.arena.allocator());
+    try index_t.set(".header", .{ .String = "Hi" });
+    try index_t.set(".rows", .{ .Embed = &login_t });
+    try index_t.set(".header", .{ .String = "Header" });
+    try index_t.set(".some-class", .{ .String = "card" });
 
-    try row_t.set("something", .{ .Value = "blooger" });
+    try row_t.set(".something", .{ .String = "blooger" });
 }
