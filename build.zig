93bcf8e8108afe92a3c611ea8bd3383bb994cbe4
Riley Fischer <rfmineguy@gmail.com>
2026-09-01 10:08:11 -0700

    implement tests using test logger

--- build.zig
+++ build.zig
@@ -7,6 +7,7 @@ pub fn build(b: *std.Build) void {
     const mod = b.addModule("zig_template", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const exe = b.addExecutable(.{
@@ -34,19 +35,34 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
+    const test_mod = b.addModule("tests", .{
+        .root_source_file = b.path("src/tests/logger.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
+    const basic_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/tests/basic/test.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{
+                    .name = "zt",
+                    .module = mod,
+                },
+                .{
+                    .name = "tests",
+                    .module = test_mod,
+                }
+            },
+        }),
     });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    
+    basic_tests.root_module.addIncludePath(b.path("src/tests/basic"));
+    
+    const run_basic = b.addRunArtifact(basic_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    test_step.dependOn(&run_basic.step);
 }
---
+++ src/tests/basic/test.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const testing = std.testing;
+const zt = @import("zt");
+const TestCtx = @import("tests").TestCtx;
+
+test "load" {
+    var ctx = try TestCtx.init(std.testing.io, "load");
+    defer ctx.deinit();
+    var logger = try ctx.logger();
+
+    {
+        try TestCtx.begin(&logger, "load ./template.htpl");
+        defer TestCtx.end(&logger) catch {};
+        const tpl = @embedFile("./template.htpl");
+        var tpl_ = try zt.Template.load(tpl, testing.io, testing.allocator);
+        defer tpl_.unload();
+        var it = tpl_.slots_map.iterator();
+        while (it.next()) |v| {
+            try TestCtx.log(&logger, "{s}: {any}", .{v.key_ptr.*, v.value_ptr.*});
+        }
+    }
+}
+
+test "set" {
+    var ctx = try TestCtx.init(std.testing.io, "set");
+    defer ctx.deinit();
+    var logger = try ctx.logger();
+
+    {
+        try TestCtx.begin(&logger, "set missing .some-class");
+        defer TestCtx.end(&logger) catch {};
+
+        const row = @embedFile("./template.htpl");
+        var index = try zt.Template.load(row, testing.io, testing.allocator);
+        defer index.unload();
+
+        try testing.expectError(error.NoSlotWithId, index.set("adf", .{ .String = "hello" }));
+        try index.set(".some-class", .{ .String = "Hello" });
+    }
+}
+
+test "generate" {
+    var ctx = try TestCtx.init(std.testing.io, "generate");
+    defer ctx.deinit();
+    var logger = try ctx.logger();
+    defer logger.flush() catch {};
+    defer logger.file.close(std.testing.io);
+
+    {
+        const index = @embedFile("./template.htpl");
+        var index_t = try zt.Template.load(index, testing.io, testing.allocator);
+        defer index_t.unload();
+
+        {
+            try TestCtx.begin(&logger, "generate ./template.htpl (see output aritifact)");
+            defer TestCtx.end(&logger) catch {};
+
+            try TestCtx.log(&logger, "  generating file...\n", .{});
+            defer TestCtx.log(&logger, "  generated file...\n", .{}) catch {};
+
+            var buf: [1024]u8 = undefined;
+            var writer = try ctx.file("template.generated.html", &buf);
+            defer writer.file.close(testing.io);
+            defer writer.flush() catch {};
+            try index_t.generate(&writer.interface);
+        }
+    }
+}
+
+test "generate w/ data" {
+    var ctx = try TestCtx.init(std.testing.io, "generate_w_data");
+    defer ctx.deinit();
+    var logger = try ctx.logger();
+    defer logger.flush() catch {};
+    defer logger.file.close(std.testing.io);
+
+    const User = struct {
+        name: []const u8,
+        age: u32,
+    };
+
+    const user: User = .{
+        .name = "Someone",
+        .age = 13,
+    };
+
+    const index = @embedFile("./template.htpl");
+    var index_t = try zt.Template.load(index, testing.io, testing.allocator);
+    defer index_t.unload();
+
+    const login = @embedFile("./data_template.htpl");
+    var login_t = try zt.Template.load(login, testing.io, testing.allocator);
+    defer login_t.unload();
+
+    try login_t.set(".username", .{ .String = user.name });
+    try login_t.set(".age",      .{ .Number = user.age });
+    try index_t.set(".rows",     .{ .Embed = &login_t });
+    
+    try TestCtx.begin(&logger, "generate ./template.htpl with embed ./data_template.htpl (see output artifact)");
+    defer TestCtx.end(&logger) catch {};
+
+    {
+        var buf: [1024]u8 = undefined;
+        var writer = try ctx.file("template.actual.generate_w_data.html", &buf);
+        defer writer.file.close(testing.io);
+        defer writer.flush() catch {};
+        try index_t.generate(&writer.interface);
+    }
+
+    try ctx.copyToTestDir("template.expected.generate_w_data.html", @embedFile("./template.expected.generate_w_data.html"));
+
+    ctx.expectFilesEqual("template.expected.generate_w_data.html", "template.actual.generate_w_data.html") catch {
+        try TestCtx.log(&logger, "  generated and expected files do not match", .{});
+        return error.MismatchedOutputs;
+    };
+}
